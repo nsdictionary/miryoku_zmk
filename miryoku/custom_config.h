fc1202aed07e0f837466cceb684efcfd506233ac
sam <3161728@gmail.com>
2024-10-21 21:42:54 +0900

    Test combo keys

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -1,3 +1,16 @@
 // Copyright 2021 Manna Harbour
 // https://github.com/manna-harbour/miryoku
 
+#define MIRYOKU_ALPHAS_QWERTY
+#define MIRYOKU_CLIPBOARD_WIN
+
+/ {
+    combos {
+        compatible = "zmk,combos";
+        combo_esc {
+            timeout-ms = <50>;
+            key-positions = <0 1>;
+            bindings = <&kp ESC>;
+        };
+    };
+};
