1215756a009fa701da88859ee3e5d8b5d656ac80
sam <3161728@gmail.com>
2024-10-21 22:04:13 +0900

    Remove combo key

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -3,14 +3,3 @@
 
 #define MIRYOKU_ALPHAS_QWERTY
 #define MIRYOKU_CLIPBOARD_WIN
-
-/ {
-    combos {
-        compatible = "zmk,combos";
-        combo_esc {
-            timeout-ms = <50>;
-            key-positions = <0 1>;
-            bindings = <&kp ESC>;
-        };
-    };
-};
